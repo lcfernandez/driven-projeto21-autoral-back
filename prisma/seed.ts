bb4ac9fcb96cbddd9e2ac2830b27d459252f85e5
lcfernandez <lcfernandez@cos.ufrj.br>
2023-04-15 18:56:59 -0300

    build: implement prisma seed

---
+++ prisma/seed.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../src/configs/database.js"
+
+async function main() {
+  await prisma.status.createMany({
+    data: [
+      { name: "Planejamento" },
+      { name: "Andamento"},
+      { name: "Pausa" },
+      { name: "Concluído" }
+    ]
+  })
+}
+
+main()
+  .then(() => {
+    console.log("Registros feitos com sucesso!")
+  })
+  .catch(e => {
+    console.error(e);
+    process.exit
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
